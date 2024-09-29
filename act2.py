514be865b6c5b875b762fb23675e805c4b2b9077
KINGROCKY2013 <r.unnithan@aris.edu.gh>
2024-09-29 11:19:20 +0000

    Add files via upload

---
+++ act2.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age:"))
+if age>=10:
+    if age<=20:
+        print ("Your age is : ", age ,"\n you are allowed to enter te class")
+    else:
+        print ("Your age is : ", age,"\n you are not allowed to enter the class")
+else:
+    print("Your age is : ", age,"\n you are not eligible")
---
+++ act3.py
@@ -0,0 +1,6 @@
+# input number greater than 1
+n = int(input("Enter the value of n: "))
+
+# loop to print numbers
+for i in range(1,n+1,1):
+        print(i)
